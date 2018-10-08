f2d0e81fff159db0dc2daf19d227e47cfa4cd251
Pavel Alimpiev <flammeusvulpes@gmail.com>
2018-10-08 13:48:43 +0300

    Use cookies to hide modal window for users that already saw it

--- js/countdownShow.js
+++ js/countdownShow.js
@@ -1,9 +1,9 @@
 define('countdownShow',function(){
-   
-    $("#modal").modal("show");
-
-    window.setTimeout(function () {
-        $("#modal").modal("hide");
-    },8000);
-    
+    if(Cookies.get('modal-was-shown') === undefined){
+        $("#modal").modal("show");
+        window.setTimeout(function () {
+            $("#modal").modal("hide");
+        },8000);
+        Cookies.set('modal-was-shown', true, { expires: 7 });
+    }
 });
