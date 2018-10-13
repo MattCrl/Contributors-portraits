6c9a4b5206a074d688b8647641b1d4d8e8ca2f37
carlosfrontend <carlosfrontend@hotmail.com>
2018-10-13 17:59:03 +0200

    Solved media querie for footer

--- js/arrow.js
+++ js/arrow.js
@@ -1,18 +1,20 @@
 define('arrow', function () {
+
     $('#arrow').hover(
         function () { $(this).removeClass('animated') },
         function () { $(this).addClass('animated') }
     );
 
-    $(window).scroll(function(){
+    $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
             $('#arrow').fadeIn();
         } else {
             $('#arrow').fadeOut();
+            $(this).off('mouseenter mouseleave');
         }
     });
 
-    $('#arrow').on('click',function(){
+    $('#arrow').on('click', function () {
         $('html, body').animate({
             scrollTop: "0px"
         }, 800);
